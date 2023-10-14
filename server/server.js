141c4ad238d26b0acb969b4104fdcea0d45122b4
gitfaizankhan <faizankhaninfo9@gmail.com>
2023-10-14 12:13:43 +0530

    backend mongodb server connected successfully

---
+++ server/server.js
@@ -0,0 +1,37 @@
+// const express = require('express');
+
+import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
+import connect from './database/conn.js';
+
+const app = express();
+
+// middlewere
+
+app.use(express.json());
+app.use(cors());
+app.use(morgan('tiny'));
+app.disable('x-power-by'); //less hackers know about our stack
+
+const port = 8080;
+
+
+// http get request;
+app.get('/', (req, res)=>{
+    res.status(201).json("Home Get Requet");
+});
+
+// start server only when we have valid connection
+connect().then(()=>{
+    try{
+        app.listen(port, () => {
+            console.log(`Server connected to http://localhost:${port}`);
+        });
+    } catch(error) {
+        console.log("Connot connecct to the server");
+    }
+}).catch(error =>{
+    connect.log("Invalid database connection....");
+})
+
